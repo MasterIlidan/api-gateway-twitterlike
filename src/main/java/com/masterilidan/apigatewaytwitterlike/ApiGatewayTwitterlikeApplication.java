9730281a78581e657cf7ae455717f8973c79d4e2
MasterIlidan <segamil2001@gmail.com>
2025-10-22 15:29:59 +0500

    API Gateway для subs service

---
+++ src/main/java/com/masterilidan/apigatewaytwitterlike/ApiGatewayTwitterlikeApplication.java
@@ -0,0 +1,49 @@
+package com.masterilidan.apigatewaytwitterlike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayTwitterlikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayTwitterlikeApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        String subsServiceUrl = "http://192.168.0.179:8082";
+        String messageServiceUrl = "http://192.168.0.179:8081";
+        String userServiceUrl = "http://192.168.0.179:8080";
+        return builder.routes()
+                //--------------------
+                //subscription service
+                //--------------------
+                .route(p -> {
+                    String path = "/subscription/follower{userId}";
+                    return p
+                            .path(path)
+                            .filters(f -> f.setPath(path))
+                            .uri(subsServiceUrl);
+                })
+                .route(p -> {
+                    String path = "/subscription";
+                    return p
+                            .path(path)
+                            .filters(f -> f.setPath(path))
+                            .uri(subsServiceUrl);
+                })
+                .route(p -> {
+                    String path = "/subscription/publisher{userId}";
+                    return p
+                            .path(path)
+                            .filters(f -> f.setPath(path))
+                            .uri(subsServiceUrl);
+                })
+
+                .build();
+    }
+}
---
+++ src/test/java/com/masterilidan/apigatewaytwitterlike/ApiGatewayTwitterlikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masterilidan.apigatewaytwitterlike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayTwitterlikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
